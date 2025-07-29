de3d44b2d56f50a453b50e95f938cfade66c6865
anandacdr <jazzw815@gmail.com>
2025-07-29 14:43:03 +0545

    📝 Update structured output demo

--- 03_Structured Output/01_typed_dict_demo.py
+++ 03_Structured Output/01_typed_dict_demo.py
@@ -1,13 +1,10 @@
 from typing import TypedDict
 
-class Person(TypeDict):
+class Person(TypedDict):
     name: str
     age: int
     email: str
 
-person = Person(name="Pikachu", age=30, email="pikachu@gmail.com")
+new_person: Person = {'name': 'Pikachu', 'age': 10, 'email': 'pikachu@gmail.com'}
 
-print(person)
-
-def get_person_info(person: Person) -> str:
-    return f"Name: {person.name}, Age: {person.age}, Email: {person.email}"
+print(new_person)
---
+++ 03_Structured Output/02_with_structured_output_typeddict.py
@@ -0,0 +1,8 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+
